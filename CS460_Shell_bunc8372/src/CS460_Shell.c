ab368816effd2c3088d54d472f46265576bf8071
bunc8372 <bunc8372@pacificu.edu>
2020-03-02 16:37:42 -0800

    .C file with empty main and comments

---
+++ CS460_Shell_bunc8372/src/CS460_Shell.c
@@ -0,0 +1,15 @@
+//****************************************************
+// File name:  CS460_SysCalls.c
+// Author:     Julian Bunch
+// Date:       02/12/2020
+// Class:      CS460
+// Assignment: SysCalls
+// Purpose:    Practice using system calls
+//****************************************************
+
+int main(int argc, char *argv[])
+{
+
+
+	return 0;
+}
