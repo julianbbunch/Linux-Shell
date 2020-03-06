e9581af0858123c4e006fa1d2255e77b0e5be40d
bunc8372 <bunc8372@pacificu.edu>
2020-03-06 10:03:36 -0800

    Command struct written

--- CS460_Shell_bunc8372/src/CS460_Shell.c
+++ CS460_Shell_bunc8372/src/CS460_Shell.c
@@ -7,9 +7,44 @@
 // Purpose:    Practice using system calls
 //****************************************************
 
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+typedef struct Command
+{
+	char* command;
+	char* arguments;
+	char* input;
+	char* outout;
+	bool bPipe;
+	bool bBackground;
+
+} Command;
+
 int main(int argc, char *argv[])
 {
+	const size_t MAX_COMMAND_SIZE = 2048;
+	const char* EXIT = "exit";
 
+	Command* psCommands;
+	int pid = getpid();
+	char *szArgs = malloc(MAX_COMMAND_SIZE);
+
+	do
+	{
+		memset(szArgs, '\0', MAX_COMMAND_SIZE);
+
+		printf("%d>", pid);
+		getline(&szArgs, (size_t*)&MAX_COMMAND_SIZE, stdin);
+		printf("%s", szArgs);
+
+
+	}while (strcmp(szArgs, EXIT) != 0);
+
+	free(szArgs);
 
 	return 0;
 }
